63a586af56dad0e7f823f56b24adeaf4e304b234
Sammi Fux <sammi.fux@student.ehb.be>
2013-12-19 15:41:09 +0100

    Use MT4j Project as source

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+import org.mt4j.MTApplication;
+
+
+public class Test extends MTApplication {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void startUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
