8cabbb37297338accc180533b5a56d9fdb45e2af
SebastianBaro <sebastian.baro@student.ehb.be>
2014-01-09 15:26:38 +0100

    Revert 8dc3f99..0734d89
    This rolls back to commit 8dc3f990b304de022f0f4313cdbac890c713c37b.

--- src/com/ehb/multec/ninjanieuws/groep7/featured/Featured.java
+++ src/com/ehb/multec/ninjanieuws/groep7/featured/Featured.java
@@ -3,56 +3,68 @@ package com.ehb.multec.ninjanieuws.groep7.featured;
 import java.util.ArrayList;
 
 import org.mt4j.MTApplication;
-import org.mt4j.components.MTComponent;
 import org.mt4j.components.TransformSpace;
+import org.mt4j.components.visibleComponents.shapes.MTRectangle;
+import org.mt4j.components.visibleComponents.widgets.MTImage;
+import org.mt4j.input.gestureAction.DefaultDragAction;
+import org.mt4j.input.gestureAction.InertiaDragAction;
 import org.mt4j.input.inputProcessors.IGestureEventListener;
 import org.mt4j.input.inputProcessors.MTGestureEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragProcessor;
 import org.mt4j.input.inputProcessors.componentProcessors.tapProcessor.TapEvent;
 import org.mt4j.input.inputProcessors.componentProcessors.tapProcessor.TapProcessor;
 import org.mt4j.sceneManagement.AbstractScene;
 import org.mt4j.util.math.Vector3D;
-import org.mt4j.util.math.Vertex;
 
-import processing.core.PImage;
-
-import com.ehb.multec.ninjanieuws.groep7.movies.NinjaMovie;
-import com.ehb.multec.ninjanieuws.groep7.movies.NinjaThumb;
-import com.ehb.multec.ninjanieuws.groep7.xml.Xml_Movie_Reader;
-
-public class Featured extends MTComponent{
+public class Featured {
 	
 	private MTApplication mtApplication;
 	private AbstractScene scene;
-	private ArrayList<String> thumbPaths;
-	private ArrayList<String> thumbTitles;
-
+	
+	
 	public void setScene(AbstractScene scene) {
 		this.scene = scene;
 	}
 
 	private int startPos, y;
 	private String imagePath =  "data" + MTApplication.separator + "images" + MTApplication.separator;
-	final String moviePath = "data" + MTApplication.separator + "movies" + MTApplication.separator;
-	
+
 	public Featured(MTApplication mtApplication, int startPos, int y) {
-		super(mtApplication);
 		this.mtApplication = mtApplication;
 		this.startPos = startPos;
 		this.y = y;
-		drawFSection();
 	}
 	
 	public void drawFSection() {
-		Xml_Movie_Reader movieXmlReader = new Xml_Movie_Reader(mtApplication);
-		thumbPaths = movieXmlReader.getNinjaMoviePaths();
-        thumbTitles = movieXmlReader.getNinjaMovieTitles();
+		ArrayList<String> photoNames = new ArrayList<String>(); 
+        
+		photoNames.add("1.jpg"); 
+        photoNames.add("2.jpg"); 
+        photoNames.add("3.jpg"); 
+        photoNames.add("4.jpg"); 
+        photoNames.add("5.jpg"); 
+        photoNames.add("6.jpg"); 
+        photoNames.add("7.jpg"); 
+        photoNames.add("8.jpg");
+        photoNames.add("9.jpg");
+        photoNames.add("10.jpg");
+        photoNames.add("11.jpg");
+        photoNames.add("12.jpg");
+        photoNames.add("13.jpg");
+        photoNames.add("14.jpg");
+        /*photoNames.add("15.jpg");
+        photoNames.add("16.jpg");
+        photoNames.add("17.jpg");
+        photoNames.add("18.jpg");*/
+        
         
         float width = 0;
         float height = 0;
         int x = startPos;
         int margin = 0;
         
-        for(int i = 0; i < thumbPaths.size(); i++){
+        
+        for(int i = 0; i < photoNames.size(); i++){
         	
         	if(x > (400 + startPos) && i < 4){
         		y += height + margin; 
@@ -75,15 +87,14 @@ public class Featured extends MTComponent{
         		y += height + margin;
         		x = startPos;
         	}
-        	PImage thumb = mtApplication.loadImage(imagePath + thumbPaths.get(i)+".jpg");
-        	final NinjaThumb ninjaThumb = new NinjaThumb(thumb, mtApplication);
-        	ninjaThumb.setTitle(thumbTitles.get(i));
-        	ninjaThumb.setPath(thumbPaths.get(i));
+        	
+        	final MTRectangle photoRect = new MTRectangle(mtApplication.loadImage(imagePath + photoNames.get(i)),mtApplication);
         	//final MTImage photoRect = new MTImage(mtApplication.loadImage(imagePath + photoNames.get(i)),mtApplication);
         	
+        	
         	if(i == 0) {
-        		width = ninjaThumb.getWidthXY(TransformSpace.GLOBAL);
-        		height = ninjaThumb.getHeightXY(TransformSpace.GLOBAL);
+        		width = photoRect.getWidthXY(TransformSpace.GLOBAL);
+        		height = photoRect.getHeightXY(TransformSpace.GLOBAL);
         	}
       
         	if(i == 1) {
@@ -97,33 +108,27 @@ public class Featured extends MTComponent{
         		height *= 2;
         	}
         	
-        	ninjaThumb.setWidthLocal(width);
-        	ninjaThumb.setHeightLocal(height);
-        	ninjaThumb.setPositionGlobal(new Vector3D(x,y,0));
+        	photoRect.setWidthLocal(width);
+        	photoRect.setHeightLocal(height);
+        	photoRect.setPositionGlobal(new Vector3D(x,y,0));
     		//double tap
-        	ninjaThumb.registerInputProcessor(new TapProcessor(mtApplication, 25, true, 350));
+    		photoRect.registerInputProcessor(new TapProcessor(mtApplication, 25, true, 350));
     		
-    		final Vector3D position = ninjaThumb.getCenterPointGlobal();
+    		final Vector3D position = photoRect.getCenterPointGlobal();
     		
-    		ninjaThumb.addGestureListener(TapProcessor.class, new IGestureEventListener() {
+    		photoRect.addGestureListener(TapProcessor.class, new IGestureEventListener() {
     			public boolean processGestureEvent(MTGestureEvent ge) {
     				TapEvent te = (TapEvent)ge;
     				if (te.isDoubleTap()){
-    					if (position != ninjaThumb.getCenterPointGlobal())
-    						ninjaThumb.setPositionGlobal(new Vector3D(position));
-    				}
-    				if(te.isTapped()){
-    					NinjaMovie ninjaMovie = new NinjaMovie(moviePath+ninjaThumb.getPath()+".mp4", new Vertex(position.x, position.y), mtApplication);
-    					scene.getCanvas().addChild(ninjaMovie);
+    					if (position != photoRect.getCenterPointGlobal())
+    						photoRect.setPositionGlobal(new Vector3D(position));
     				}
     				return false;
     			}
     		});
-    		
-    		
-    		ninjaThumb.setStrokeWeight(0);
-    		ninjaThumb.setNoStroke(true);
-        	this.addChild(ninjaThumb);
+    		photoRect.setStrokeWeight(0);
+        	photoRect.setNoStroke(true);
+        	scene.getCanvas().addChild(photoRect);
         	x += width + margin;
         }
 	}
